c3a91be6a043f04046d07654f9126013dc34e402
Fly J Gore <jennifer@theothergore.com>
2017-11-13 18:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 currentUser = "Nash Gore";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${currentUser}, {"!"}";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, currentUser, ${"!"}";
